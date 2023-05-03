a9ce456cf141300cd6f57d89dae7bcf7738f9780
BuildTools <unconfigured@null.spigotmc.org>
2023-05-03 12:24:56 -0300

    conectado saludo y about en portfolio sin loggin

--- src/app/components/home2/about2/about2.component.ts
+++ src/app/components/home2/about2/about2.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AnalyticsService } from 'src/app/services/analytics/analytics.service';
+import { CrudService } from 'src/app/services/CrudService/crud-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about2',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about2.component.scss']
 })
 export class About2Component implements OnInit {
-
-  constructor() { }
+  personas: any;
+  aboutDesc: any;
+  constructor(public analyticsService: AnalyticsService,private crudService: CrudService, private router: Router) {}
 
   ngOnInit(): void {
+    this.getItems();
+    this.getAboutDesc();
+  }
+  getItems(): void {
+    this.crudService.getItems().subscribe((data) => {
+      this.personas = data;
+    });
   }
 
+  editItem(itemId: number): void {
+    this.router.navigate(['/edit', itemId]);
+  }
+
+  deleteItem(itemId: number): void {
+    this.crudService.deleteItem(itemId).subscribe(() => {
+      this.getItems(); 
+    });
+  }
+  getAboutDesc() {
+    this.crudService.getAboutDesc().subscribe(aboutDesc => {
+      this.aboutDesc = aboutDesc;
+    });
+  }
+  goToEditAbout() {
+    this.router.navigate(['/edit/about']);
+  }
 }
--- src/app/components/home2/contact2/contact2.component.ts
+++ src/app/components/home2/contact2/contact2.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AnalyticsService } from 'src/app/services/analytics/analytics.service';
+import { CrudService } from 'src/app/services/CrudService/crud-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact2',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact2.component.scss']
 })
 export class Contact2Component implements OnInit {
-
-  constructor() { }
+  personas: any;
+  saludoDesc: any;
+  constructor(public analyticsService: AnalyticsService,private crudService: CrudService, private router: Router) {}
 
   ngOnInit(): void {
+    this.getItems();
+    this.getSaludoDesc();
+  }
+  getItems(): void {
+    this.crudService.getItems().subscribe((data) => {
+      this.personas = data;
+    });
   }
 
+  editItem(itemId: number): void {
+    this.router.navigate(['/edit', itemId]);
+  }
+
+  deleteItem(itemId: number): void {
+    this.crudService.deleteItem(itemId).subscribe(() => {
+      this.getItems(); 
+    });
+  }
+  getSaludoDesc() {
+    this.crudService.getSaludoDesc().subscribe(saludoDesc => {
+      this.saludoDesc = saludoDesc;
+    });
+  }
+  goToEditSaludo() {
+    this.router.navigate(['/edit/saludo']);
+  }
 }
